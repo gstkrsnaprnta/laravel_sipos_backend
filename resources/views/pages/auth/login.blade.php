0b92987ba155081d7a4a0073e1a457356b629ab5
19rarisja <Arisja Rahim>
2024-10-06 14:17:44 +0800

    login warning

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -16,38 +16,35 @@
 
         <div class="card-body">
             <form method="POST" action="{{route('login')}} "class="needs-validation" novalidate="">
+                @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input id="email" type="email" class="form-control @error('email')
+                    <input id="email" type="email"
+                        class="form-control @error('email')
                         is-invalid
-                    @enderror" name="email" tabindex="1"
-                        autofocus>
-                        @error('email')
+                    @enderror"
+                        name="email" tabindex="1" autofocus>
+                    @error('email')
                         <div class="invalid-feedback">
                             {{$message}}
                         </div>
-                            @enderror
+                    @enderror
 
                 </div>
 
                 <div class="form-group">
                     <div class="d-block">
                         <label for="password" class="control-label ">Password</label>
-                        <div class="float-right">
-                            <a href="auth-forgot-password.html"
-                                class="text-small">
-                                Forgot Password?
-                            </a>
-                        </div>
+
                     </div>
                     <input id="password" type="password" class="form-control @error('password')
                         is-invalid
                     @enderror" name="password" tabindex="2">
                     @error('password')
-                    <div class="invalid-feeedback">
+                    <div class="invalid-feedback">
                         {{$message}}
                     @enderror
-                  
+
                 </div>
 
                 <div class="form-group">
