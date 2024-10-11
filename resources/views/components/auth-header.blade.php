ca2c764ce2a6d15a50fd027dffc16571a06d1706
19rarisja <rarisja@gmail.com>
2024-10-12 07:20:40 +0800

    Ubah logo stisla menjadi sipos

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,6 +1,5 @@
- <div class="login-brand">
-     <img src="{{ asset('img/stisla-fill.svg') }}"
-         alt="logo"
-         width="100"
-         class="shadow-light rounded-circle">
- </div>
+<div class="login-brand">
+    <img src="{{ asset('img/logo.png') }}" 
+         alt="logo" 
+         style="width: 250px; height: 80px;">
+</div>
