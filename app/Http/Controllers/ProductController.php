9d90a161f46eab251ad0327980ed482c634cc1a6
19rarisja <rarisja@gmail.com>
2024-10-13 03:56:05 +0800

    Menambahkan fitur CRUD untuk produk

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; 
+
+
+class ProductController extends Controller
+{
+    public function index(Request $request)
+    {
+        //get data products
+        $products = DB::table('products')
+            ->when($request->input('name'), function ($query, $name) {
+                return $query->where('name', 'like', '%' . $name . '%');
+            })
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        //sort by created_at desc
+
+        return view('pages.products.index', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('pages.products.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validasi data yang dikirimkan
+        $validated = $request->validate([
+            'name' => 'required|min:3|unique:products',
+            'price' => 'required|integer',
+            'stock' => 'required|integer',
+            'category' => 'required|in:food,drink,snack',
+            'image' => 'required|image|mimes:png,jpg,jpeg'
+        ]);
+    
+        // Simpan file gambar dengan nama unik
+        $filename = time() . '.' . $request->image->extension();
+        $request->image->storeAs('public/products', $filename);
+    
+        // Buat instance produk baru
+        $product = new \App\Models\Product;
+        $product->name = $validated['name'];
+        $product->price = (int) $validated['price'];
+        $product->stock = (int) $validated['stock'];
+        $product->category = $validated['category'];
+        $product->image = $filename;
+        $product->save();
+    
+        // Redirect ke halaman index dengan pesan sukses
+        return redirect()->route('product.index')->with('success', 'Product successfully created');
+    }
+    
+
+    public function edit($id)
+    {
+        $product = \App\Models\Product::findOrFail($id);
+        return view('pages.products.edit', compact('product'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $product = \App\Models\Product::findOrFail($id);
+        $product->update($data);
+        return redirect()->route('product.index')->with('success', 'Product successfully updated');
+    }
+
+    public function destroy($id)
+    {
+        $product = \App\Models\Product::findOrFail($id);
+        $product->delete();
+        return redirect()->route('product.index')->with('success', 'Product successfully deleted');
+    }
+}
