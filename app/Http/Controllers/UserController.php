c52a5573edc1ed9455d84ac4e21c435d1abcc21c
“gstkrsnaprnta” <gstkrisnapranata@gmail.com>
2024-10-13 04:17:57 +0800

    Refactor UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,36 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use Illuminate\Support\Facades\DB; 
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\UpdateUserRequest;
-use App\Models\User;
+
 
 class UserController extends Controller
 {
-    public function index(Request $request){
-    // Menggunakan query builder dengan when dan paginate
+    public function index(Request $request)
+    {
+        // Menggunakan query builder dengan when dan paginate
         $users = DB::table('users')
-        ->when($request->input('name'), function ($query, $name) {
-            return $query->where('name', 'like', '%' . $name . '%');
-        })
-        ->orderBy('id', 'desc')
-        ->paginate(10);
+            ->when($request->input('name'), function ($query, $name) {
+                return $query->where('name', 'like', '%' . $name . '%');
+            })
+            ->orderBy('id', 'desc')
+            ->paginate(10);
 
-      return view('pages.users.index', compact('users'));
-       
+        return view('pages.users.index', compact('users'));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('pages.users.create');
     }
 
-    public function store(Request $request){
-
+    public function store(Request $request)
+    {
         $data = $request->all();
         $data['password'] = Hash::make($request->password);
         \App\Models\User::create($data);
-        return redirect()->route('user.index')->with('success', 'User berhasil ditambahkan');
+        return redirect()->route('user.index')->with('success', 'User successfully created');
     }
 
     public function edit($id)
@@ -53,5 +55,4 @@ class UserController extends Controller
         $user->delete();
         return redirect()->route('user.index')->with('success', 'User successfully deleted');
     }
-
 }
